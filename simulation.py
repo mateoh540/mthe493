889b194bc6f37b9163a173af5ef80f4bc7b09d08
Mateo Huster <mateo.huster@gmail.com>
2025-10-18 15:39:29 -0400

    initial

---
+++ simulation.py
@@ -0,0 +1,8 @@
+# Libraries
+import networkx as nx   # For Graphs
+import matplotlib.pyplot as plt # For Plotting
+import matplotlib.colors as mcolors
+from matplotlib.animation import FuncAnimation
+import math
+import random
+
